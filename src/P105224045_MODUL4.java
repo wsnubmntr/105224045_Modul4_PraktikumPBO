7ef1a64e483dfead690c12a8be127d2b49cd3568
wsnubmntr <nuuu008@gmail.com>
2026-03-31 14:47:52 +0700

    modul4

--- src/P105224045_MODUL4.java
+++ src/P105224045_MODUL4.java
@@ -1,14 +1,3 @@
-class Mahasiswa {
-    String nama;
-    int umur;
-    String jurusan;
-
-    void tampilkanData() {
-        System.out.println("Nama: " + nama);
-        System.out.println("Umur: " + umur);
-        System.out.println("Jurusan: " + jurusan);
-    }
-}
 
 public class P105224045_MODUL4 { 
     public static void main(String[] args) {
---
+++ src/mahasiswa.java
@@ -0,0 +1,11 @@
+class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusan;
+
+    void tampilkanData() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
